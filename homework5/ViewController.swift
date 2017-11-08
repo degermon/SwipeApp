295415e0c1d70b996c0995cfe28ca05a1978367a
Daniel Suskevic <dsuskevic@klt-applemacmookpro1.local>
2017-11-08 20:33:29 +0200

    Initial Commit

--- homework5/ViewController.swift
+++ homework5/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var number : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        number = number - 1
+        label.text = "\(number)"
+    }
+    
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        number = number + 1
+        label.text = "\(number)"
+    }
+    
 }
 
